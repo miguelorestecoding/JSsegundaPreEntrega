3018131fa347e383cecfd7e0e42636c1ac63b8a0
miguelorestecoding <miguelorestecoding@gmail.com>
2023-03-10 21:08:37 -0300

    trabajando en la funcion sumaImpuestosAlDolar

--- scriptV01.js
+++ scriptV01.js
@@ -1,84 +1,102 @@
 // Variables Globales
-let impuestosAlDolar = []
+let impuestosAlDolar = [];
+let tiposDeDolar = [];
+let textoListaImpuestosAgregados = `Impuestos Definidos:\n`;
 
 class ImpuestosAlDolar {
-    constructor(nombre, factor) {
-        this.nombre = nombre;
-        this.factor = factor;
-    }
+  constructor(nombre, factor) {
+    this.nombre = nombre;
+    this.factor = factor;
+  }
 }
 
-let tiposDeDolar = []
-
 class TiposDeDolar {
-    constructor(nombre) {
-        this.nombre = nombre;
-    }
+  constructor(nombre, factor) {
+    this.nombre = nombre;
+    this.factor = factor;
+  }
+}
+
+// seguir desde aca, que al tocar 1 o 2 o 3 se vaya sumando el factor del impuesto a la variable factor total.
+function sumaImpuestosAlDolar() {
+  let factorTotal = "";
+  let suma = prompt(
+    `Ingresa que impuestos se aplican al dolar que estas creando (Cancelar para Terminar):\n ${textoListaImpuestosAgregados}`
+  );
 }
 
 function menuPrincipal() {
+  let opcionMenuPrincipal = "";
 
-    let opcionMenuPrincipal = ""
+  opcionMenuPrincipal = prompt(
+    `Elige la opcion:\n1 - Crear Impuesto\n2 - Ver Impuestos Existentes\n3 - Crear Nuevo Tipo de Dolar\n4 - Ver tipo de Dolares Existentes\n5 - Utilizar el Calculador del Dolar\n6 - Salir del Aplicativo `
+  );
 
-    opcionMenuPrincipal = prompt(`Elige la opcion:\n1 - Crear Impuesto\n2 - Ver Impuestos Existentes\n3 - Crear Nuevo Tipo de Dolar\n4 - Ver tipo de Dolares Existentes\n5 - Utilizar el Calculador del Dolar\n6 - Utilizar el Calculador del Dolar\n7 - Salir del Aplicativo `)
+  if (opcionMenuPrincipal == null) {
+    alert(
+      "⛔ En el Menu Principal has presionado el botón cancelar.\n👋 Adios, vuelve cuando Quieras! "
+    );
+    return;
+  }
 
-    if (opcionMenuPrincipal == null) {
-        alert(
-          "⛔ En el Menu Principal has presionado el botón cancelar.\n👋 Adios, vuelve cuando Quieras! "
-        );
-        return;
-      }
-
-    switch(parseInt(opcionMenuPrincipal)) {
-        case 1:
-        agregarTipoDeImpuestos()
-        menuPrincipal()
-        break;
-        case 2:
-        alert(mostrarTiposDeImpuestos(impuestosAlDolar))
-        menuPrincipal()
-        break;
-        case 3:
-        break;
-        case 4:
-        break;
-        case 5:
-        break;
-        case 6:
-        break;
-        default:
-            alert("🥴 Opción inválida en el Menú Principal, por favor seleccione una opción válida.");
-            menuPrincipal()
-        break;
-
-    }
+  switch (parseInt(opcionMenuPrincipal)) {
+    case 1:
+      agregarTipoDeImpuestos();
+      menuPrincipal();
+      break;
+    case 2:
+      alert(mostrarTiposDeImpuestos(impuestosAlDolar));
+      menuPrincipal();
+      break;
+    case 3:
+      sumaImpuestosAlDolar();
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      alert(
+        "⛔ En el Menu Principal has presionado la Opcion 6.\n👋 eso es un Adios! "
+      );
+      break;
+    default:
+      alert(
+        "🥴 Opción inválida en el Menú Principal, por favor seleccione una opción válida."
+      );
+      menuPrincipal();
+      break;
+  }
 }
 
 function agregarTipoDeImpuestos() {
-    
-    let nombreDeImpuesto = prompt("Ingrese el nombre del impuesto que desea agregar")
-    let factorDelImpuesto = prompt("Ingrese el porcentaje del impuesto")
+  let nombreDeImpuesto = prompt(
+    "Ingrese el nombre del impuesto que desea agregar"
+  );
+  let factorDelImpuesto = prompt("Ingrese el porcentaje del impuesto");
 
-    let impuestoACrear = new ImpuestosAlDolar(nombreDeImpuesto, (parseInt(factorDelImpuesto)/100))
-    impuestosAlDolar.push(impuestoACrear)
-    console.log(impuestosAlDolar)
+  let impuestoACrear = new ImpuestosAlDolar(
+    nombreDeImpuesto,
+    parseInt(factorDelImpuesto) / 100
+  );
+  impuestosAlDolar.push(impuestoACrear);
+  console.log(impuestosAlDolar);
 }
 
 function mostrarTiposDeImpuestos(impuestosAlDolar) {
-    let textoListaImpuestosAgregados = `Impuestos Definidos:\n`
-    for (const impuesto of impuestosAlDolar) {
-        textoListaImpuestosAgregados += `\nNombre: ${impuesto.nombre}\nFactor: ${impuesto.factor}`
-    }
-    return textoListaImpuestosAgregados
+  for (const impuesto of impuestosAlDolar) {
+    textoListaImpuestosAgregados += `\n------------------------------\n**Orden**: ${
+      impuestosAlDolar.indexOf(impuesto) + 1
+    }\n**Nombre**: ${impuesto.nombre}\n**Factor**: ${impuesto.factor}`;
+  }
+  return textoListaImpuestosAgregados;
 }
 
 function main() {
-    menuPrincipal()
+  menuPrincipal();
 }
 
-main()
-
-
+main();
 
 // class tiposDeDolar {
 //     constructor(nombre, impuestoPais, impuestoGanancias, impuestoBienesPersonales ) {
@@ -88,7 +106,6 @@ main()
 //     this.impuestoBienesPersonales = impuestoBienesPersonales
 //     }
 // }
-
 
 // const dolarTipos = [
 //     { nombre: "Dólar Solidario", factor: 1.65 },
